ec47aa419d6fcf0d7f8e6e72522bf807f03d7258
Nastya <anastasiy-ya@mail.ru>
2022-04-18 20:44:34 +0500

    Добавила init(), get_coords()

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <cctype>
+
 using std::cout;
+using std::cin;
 
 char bord[3][3];
 enum { EMPTY = ' ', CROSS ='X', ZERO = 'O' };
 
+char player_char;
+char computer_char;
+
+void init() {
+	setlocale(LC_ALL, "ru_RU");
+	do {
+		cout << "—делайте свой выбор [X/0]: ";
+		cin >> player_char;
+		player_char = toupper(player_char); //UPPER CASE
+	} while (player_char != 'X' && player_char != '0');
+	
+	computer_char = (player_char == 'X' ? '0' : 'X');
+}
+
+void get_coords(int& row, int& col) {
+	
+	do {
+		cout << "¬ведите координаты x: [1;3], y: [1;3]: ";
+		cin >> col >> row;
+		col--;
+		row--;
+	} while (col < 0 || col >2 || row < 0 || row >2 || bord[row][col]!=EMPTY);
+
+}
+
+void make_move() {
+	
+}
 
 void print(const char board[3][3]) {
 	for (int i = 0; i < 3; i++) {
@@ -24,7 +55,7 @@ void print(const char board[3][3]) {
 }
 
 
-
+// тест
 bool is_full(const char board[3][3]) {
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++)
